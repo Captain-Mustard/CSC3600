9c72dcf02064d6ef9fe32a659fba674a277e9d31
Jake Flynn <u1138451@umail.usq.edu.au>
2023-08-24 13:07:44 +1000

    further updates to get functions to connect to the db

--- Model/user_booking.php
+++ Model/user_booking.php
@@ -27,8 +27,25 @@ function insert_trip_booking($passenger_id, $passenger_type, $bus_number,
 
 # insert into passengerTracking i.e when a user books this should be ran as well.
 
-function insert_into_tracking(){
-	
+function insert_into_tracking($passenger_id, $passenger_type, $booking_date,
+							  $stop_location, $on_time, $off_time, $finised){
+	global $db;
+    $query = 'INSERT INTO PassengerTracking
+                 (passengerId, passengerType, bookingDate,  
+				  stopLocation, onTime, offTime, finished)
+              VALUES
+                 (:passenger_id, :passenger_type, :booking_date,
+				  :stop_location, :on_time, :off_time, :finished)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':passenger_id', $passenger_id);
+    $statement->bindValue(':passenger_type', $passenger_type);
+    $statement->bindValue(':booking_date', $booking_date);
+    $statement->bindValue(':stop_location', $stop_location);
+	$statement->bindValue(':on_time', $on_time);
+	$statement->bindValue(':off_time', $off_time);
+	$statement->bindValue(':finised', $finished);
+    $statement->execute();
+    $statement->closeCursor();
 	
 	
 }
@@ -88,10 +105,29 @@ function get_trip_by_stop_location($stop_location){
 
 
 # lets the bus driver mark passengers as off - like if the bus is empty
-function check_passengers_off(){
-	
-	
-	
+function check_passengers_off($passenger_id, $passenger_type, $booking_date,
+							  $stop_location, $on_time, $off_time, $finised){
+	global $db;
+	$query = 'UPDATE PassengerTracking
+              SET passengerId = :passenger_id,
+				  PassengerType = :passenger_type,
+				  bookingDate = :booking_date,
+				  stopLocation = :stop_location,
+				  onTime = :on_time,
+				  offTime = :off_time,
+				  finised = :finised
+              WHERE passengerId = :passenger_id AND bookingDate = :booking_date 
+			  AND startLocation = :stop_location'
+	$statement = $db->prepare($query);
+	$statement->bindValue(':passenger_id', $passenger_id);
+    $statement->bindValue(':passenger_type', $passenger_type);
+    $statement->bindValue(':booking_date', $booking_date);
+    $statement->bindValue(':stop_location', $stop_location);
+	$statement->bindValue(':on_time', $on_time);
+	$statement->bindValue(':off_time', $off_time);
+	$statement->bindValue(':finised', $finished);
+    $statement->execute();
+    $statement->closeCursor();
 }
 
 
