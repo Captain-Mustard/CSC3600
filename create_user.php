9bf3639f49dabc5fcd9165d8417a67980a8331fd
Jake Flynn <u1138451@umail.usq.edu.au>
2023-08-30 15:21:22 +1000

    Upload files to ''

---
+++ create_user.php
@@ -0,0 +1,40 @@
+
+
+<style>
+	
+</style>
+
+<main>
+
+    <h2>Create User</h2>
+		
+    <!-- display a search form -->
+    <form action="." method="post">
+        <label for="uni_id">University ID:</label>
+			<input type="text" id="uni_id" name="uni_id" 
+			value="" required>
+		
+		<!-- make this a drop down -->
+		<label for="uni_id">University ID:</label>
+			<input type="text" id="uni_id" name="uni_id" 
+			value="" required>
+		
+		<label for="email">Email:</label>
+			<input type="text" id="email" name="email" 
+			value="" required>
+		
+		<label for="password">Password:</label>
+			<input type="text" id="password" name="password" 
+			value="" required>
+		
+		
+		
+		
+			<input type="hidden" name="action" value="login_customer">
+                
+			<input type="submit" value="Login">
+    </form>
+	
+	
+	
+</main>
