1ad2bd8aea77e4af22eacb3bea1762ee433ebe63
Ben McKenzie <u1058913@umail.usq.edu.au>
2023-09-04 21:52:52 +1000

    Update 'Model/database.php'
    Add error catch

--- Model/database.php
+++ Model/database.php
@@ -6,7 +6,8 @@
     try {
         $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
-        
+        $error_message = $e->getMessage();
+        include('../errors/database_error.php');
         exit();
     }
 ?>
