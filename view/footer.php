764fe505fd2ec396a766b9f4ac986baf72ade1c5
Ben McKenzie <u1058913@umail.usq.edu.au>
2023-09-22 00:11:20 +1000

    Update 'view/footer.php'

--- view/footer.php
+++ view/footer.php
@@ -1,13 +1,16 @@
 
 <footer>
-    <p>
-        The University of Southern Queensland acknowledges the First Nations of southern Queensland and their 
-        ongoing connection to Country, lands, and waterways. Further, we recognise Aboriginal and Torres Strait 
-        Islander peoples as the first educators and researchers of Australia. We pay deep respect to Elders past and present.
-    </p>
-    <p class="copyright">
+    <div class="first-paragraph">
+        <p>
+            The University of Southern Queensland acknowledges the First Nations of southern Queensland and their 
+            ongoing connection to Country, lands, and waterways. Further, we recognise Aboriginal and Torres Strait 
+            Islander peoples as the first educators and researchers of Australia. We pay deep respect to Elders past and present.
+        </p>
+    </div>
+    <div class="copyright">
+        <img src="/csc3600/images/unisq-logo-acronym-white.svg" alt="USQ Logo" class="footer-logo">
         &copy; <?php echo date("Y"); ?> Jackal Software
-    </p>
+    </div>
 </footer>
 </body>
 </html>
