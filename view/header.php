393637da592321d8a6a63e013ebe792a32438cf0
Ben McKenzie <u1058913@umail.usq.edu.au>
2023-09-22 00:10:55 +1000

    Update 'view/header.php'

--- view/header.php
+++ view/header.php
@@ -8,14 +8,22 @@
           href="/CSC3600/main.css">
 </head>
 
-<!-- the body section -->
-<body>
 <header>
-    <h1>Jackal Software - Inter Campus Shuttle System</h1>
-    <p>Admin Centre</p>
-    <nav>
-        <ul>
-            <li><a href="/CSC3600/">Home</a></li>
-        </ul>
-    </nav>
+    <div class="header-left">
+        <img src="/CSC3600/images/UniSQ_Logo.png" alt="Logo">
+    </div>
+    <div class="header-center">
+        <h1>Jackal Software - Inter Campus Shuttle System</h1>
+    </div>
+    <div class="header-right">
+        <nav>
+            <ul>
+                <li><a href="/CSC3600/">Home</a></li>
+                <li><a href="/CSC3600/analytics">Analytics</a></li>
+                <li><a href="/CSC3600/booking_platform">Bookings</a></li>
+                <li><a href="/CSC3600/driver_platform">Driver</a></li>
+                <!-- Add more navigation items as needed -->
+            </ul>
+        </nav>
+    </div>
 </header>
