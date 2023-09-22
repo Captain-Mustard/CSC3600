6ef04bc34a70f97f5fdc2b2285983b2171ef5afb
Laura Betts <u1125483@umail.usq.edu.au>
2023-09-23 03:37:42 +1000

    Deleted code that was mistakenly put on the wrong page last week.
    (I got confused last time and edited the wrong page. Deleted code that was in the wrong spot).

--- booking_platform/home_page.php
+++ booking_platform/home_page.php
@@ -1,33 +1,7 @@
 <?php include '../view/header.php'; ?>
 
 <?php
-#Had bugs when trying to echo the sql values, hard-coded for now, will fix later.
-echo "Bookings: ";
-echo"Today's Date:";
-echo "<table border='1' width='800px'>";
-	echo"<tr>";
-		echo "<th>Trip ID</th>";
-		echo "<th>Time</th>";
-		echo "<th>Bus No.</th>";
-		echo "<th>Start Dest.</th>";
-		echo "<th>End Dest.</th>";
-		echo "<th>Start Time</th>";
-		echo "<th>End Time</th>";
-		echo "<th>Passenger List</th>";
-	echo"</tr>";
-	for ($i = 0;$i<=10;$i++){
-		echo"<tr>";
-		echo"<td>$i</td>";
-		echo"<td>time</td>";
-		echo"<td>bus</td>";
-		echo"<td>startdest</td>";
-		echo"<td>enddest</td>";
-		echo"<td>starttime</td>";
-		echo"<td>endtime</td>";
-		## Error -> Button below not working.
-		echo'<td><button type="button" onclick="location.href="../driver_platform/list_passengers.php"">View Passengers</button></td>';
-		echo"<tr>";
-	}
+
 ?>
 
 
