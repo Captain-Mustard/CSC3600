e4865cd0302e591367efa0e2c33569dffe021999
Ben McKenzie <u1058913@umail.usq.edu.au>
2023-09-26 23:54:23 +1000

    Update 'booking_platform/index.php'

--- booking_platform/index.php
+++ booking_platform/index.php
@@ -13,7 +13,7 @@ if ($action === NULL) {
         if (isset($_SESSION['loggedin'])) {
             // Redirect based on user type
             if (isset($_SESSION['user_type']) && $_SESSION['user_type'] == "customer") {
-                $action = 'home_page';
+                $action = 'booking_display';
             } elseif (isset($_SESSION['user_type']) && $_SESSION['user_type'] != "customer") {
                 if ($_SESSION['user_type'] == "driver") {
                     header('Location: ../driver_login/');
@@ -24,14 +24,15 @@ if ($action === NULL) {
         } else {
             // Redirect to customer login if not logged in
             header('Location: ../customer_login/');
+            exit;
         }
     }
 }
 
 // Handle actions
-if ($action == 'home_page') {
+if ($action == 'booking_display') {
     // Include the home page for customers
-    include('home_page.php');
+    include('booking_display.php');
 } elseif ($action == 'log_out') {
     // Destroy the session and redirect to customer login
     session_destroy();
