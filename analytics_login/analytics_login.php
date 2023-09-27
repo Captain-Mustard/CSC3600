4dfafaac3bcead3c5fd306a86d430bacc3573610
Ben McKenzie <u1058913@umail.usq.edu.au>
2023-09-27 23:06:25 +1000

    Update 'analytics_login/analytics_login.php'
    Validation & Styling

--- analytics_login/analytics_login.php
+++ analytics_login/analytics_login.php
@@ -1,33 +1,62 @@
 <?php include '../view/header.php'; ?>
 
-<style>
-	<?php include '../main.css';?>
-</style>
-
 <main>
+    <div class="container">
+        <h2>Administrator Login</h2>
 
-    <h2>Analytics Login</h2>
-		
-    <!-- display a search form -->
-    <form action="." method="post">
-        <label for="username">Username:</label>
-			<input type="text" id="username" name="username" 
-			value="" required>
-		
-		<label for="password">Password:</label>
-			<input type="password" id="password" name="password" 
-			value="" required>
-		
-		
-		
-		
-			<input type="hidden" name="action" value="logged_in">
-                
-			<input type="submit" value="Login">
-    </form>
-	
+        <?php
+        // Check if a success message is set in the session
+        if (isset($_SESSION['success_message'])) {
+            // Display the success message
+            echo '<div class="success-message">' . htmlspecialchars($_SESSION['success_message']) . '</div>';
 
-	
-<?php include '../view/footer.php'; ?>
-	
+            // Clear the success message from the session to prevent it from displaying again on refresh
+            unset($_SESSION['success_message']);
+        }
+
+        // Check if an error message is set
+        if (isset($error)) {
+            echo '<div class="error-message">' . htmlspecialchars($error) . '</div>';
+        }
+        ?>
+
+        <!-- Display a login form -->
+        <div class="form-container">
+            <form action="index.php" method="post" onsubmit="return validateForm()" class="user-form">
+                <div class="form-group">
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username" required class="input-field">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" required class="input-field">
+                </div>
+
+                <input type="hidden" name="action" value="logged_in">
+
+                <input type="submit" value="Login" class="submit-button">
+            </form>
+        </div>
+    </div>
 </main>
+
+<script>
+    function validateForm() {
+        document.getElementById('error_message').textContent = '';
+
+        let username = document.forms['userForm']['username'].value;
+        let password = document.forms['userForm']['password'].value;
+
+        let isValid = true;
+
+        if (username.trim() === '' || password.trim() === '') {
+            document.getElementById('error_message').textContent = 'Please enter both username and password.';
+            isValid = false;
+        }
+
+        return isValid;
+    }
+</script>
+
+<?php include '../view/footer.php'; ?>
