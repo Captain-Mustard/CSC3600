88923f945bf29ca43bdf4230363d2ba977f641dd
Ben McKenzie <u1058913@umail.usq.edu.au>
2023-09-27 23:06:47 +1000

    Update 'analytics_login/index.php'

--- analytics_login/index.php
+++ analytics_login/index.php
@@ -1,79 +1,62 @@
-<?php
-require('../model/database.php');
-require('../model/login_user.php');
-// starts login session
+<?php include '../view/header.php'; ?>
 
-session_start();
+<main>
+    <div class="container">
+        <h2>Administrator Login</h2>
 
+        <?php
+        // Check if a success message is set in the session
+        if (isset($_SESSION['success_message'])) {
+            // Display the success message
+            echo '<div class="success-message">' . htmlspecialchars($_SESSION['success_message']) . '</div>';
 
-// checks current session, if its analytics will skip login, if its a different user type, it will destroy the session and make the user sign in
-$action = filter_input(INPUT_POST, 'action');
-if ($action === NULL) {
-    $action = filter_input(INPUT_GET, 'action');
-    if ($action === NULL) {
-        if (isset($_SESSION['loggedin'])) {
-            if (isset($_SESSION['user_type']) && $_SESSION['user_type'] == "analytics") {
-                header('Location: ../analytics/');
-                
-            } elseif (isset($_SESSION['user_type']) && $_SESSION['user_type'] != "analytics") {
-                if($_SESSION['user_type'] == "customer"){
-					
-					header('Location: ../customer_login/');
-				}else if($_SESSION['user_type'] == "driver"){
-					
-					header('Location: ../driver_login/');
-				}
-                
-            }
-        } else {
-            $action = 'analytics_login';
+            // Clear the success message from the session to prevent it from displaying again on refresh
+            unset($_SESSION['success_message']);
         }
+
+        // Check if an error message is set
+        if (isset($error)) {
+            echo '<div class="error-message">' . htmlspecialchars($error) . '</div>';
+        }
+        ?>
+
+        <!-- Display a login form -->
+        <div class="form-container">
+            <form action="index.php" method="post" onsubmit="return validateForm()" class="user-form">
+                <div class="form-group">
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username" required class="input-field">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" required class="input-field">
+                </div>
+
+                <input type="hidden" name="action" value="logged_in">
+
+                <input type="submit" value="Login" class="submit-button">
+            </form>
+        </div>
+    </div>
+</main>
+
+<script>
+    function validateForm() {
+        document.getElementById('error_message').textContent = '';
+
+        let username = document.forms['userForm']['username'].value;
+        let password = document.forms['userForm']['password'].value;
+
+        let isValid = true;
+
+        if (username.trim() === '' || password.trim() === '') {
+            document.getElementById('error_message').textContent = 'Please enter both username and password.';
+            isValid = false;
+        }
+
+        return isValid;
     }
-}
+</script>
 
-// takes users to the login page
-if ($action == 'analytics_login') {
-    
-	include('analytics_login.php');
-} 
-
-// validates user login credentials
-else if($action == 'logged_in'){
-	// sets username for session after login validation
-	$username = filter_input(INPUT_POST, 'username');
-	$password = filter_input(INPUT_POST, 'password');
-	
-	
-	
-	$user_login = get_metrics_account($username);
-	if ($user_login && isset($user_login['username']) && isset($user_login['password'])) {
-    $db_username = $user_login['username'];
-    $db_password = $user_login['password'];
-
-    // sets the session
-    if (password_verify($password, $db_password)) {
-        session_regenerate_id(true); 
-        $_SESSION['loggedin'] = true;
-        $_SESSION['userid'] = $username;
-        $_SESSION['user_type'] = "analytics";
-        header('Location: ../analytics/'); 
-
-    } else {
-        $error = 'Username or Password Incorrect';
-        echo $error;
-    }
-} else {
-    $error = 'Please enter a valid username and password';
-    echo $error;
-}
-}
-
-
-
-// logs users out of session
-else if ($action == 'logged_out'){
-	session_destroy(); // This destroys the entire session
-	include('analytics_login.php');
-}
-
-?>
+<?php include '../view/footer.php'; ?>
