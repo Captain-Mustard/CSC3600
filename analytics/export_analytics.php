a7e5d4b0177d68fc7afca88d7d6d8c9d8393a786
Nathaniel <nathaniel@Ubuntu.myguest.virtualbox.org>
2023-09-29 09:01:25 +1000

    Created export fuction for analytics

---
+++ analytics/export_analytics.php
@@ -0,0 +1,57 @@
+<?php
+// Function developed to export data from a table into an excel file. Integrate with analytics pages as needed.
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "shuttle_bus";
+//mysql and db connection
+
+$con = new mysqli($servername, $username, $password, $dbname);
+
+if ($con->connect_error) {  //error check
+    die("Connection failed: " . $con->connect_error);
+}
+else
+{
+
+}
+
+
+$DB_TBLName = "your_table_name"; //Change as needed
+$filename = "excelfilename";  //your_file_name
+$file_ending = "xls";   //file_extention
+
+header("Content-Type: application/xls");    
+header("Content-Disposition: attachment; filename=$filename.'.'.$file_ending");  
+header("Pragma: no-cache"); 
+header("Expires: 0");
+
+$sep = "\t";
+
+$sql="SELECT * FROM $DB_TBLName"; 
+$resultt = $con->query($sql);
+while ($property = mysqli_fetch_field($resultt)) { //fetch table field name
+    echo $property->name."\t";
+}
+
+print("\n");    
+
+while($row = mysqli_fetch_row($resultt))  //fetch_table_data
+{
+    $schema_insert = "";
+    for($j=0; $j< mysqli_num_fields($resultt);$j++)
+    {
+        if(!isset($row[$j]))
+            $schema_insert .= "NULL".$sep;
+        elseif ($row[$j] != "")
+            $schema_insert .= "$row[$j]".$sep;
+        else
+            $schema_insert .= "".$sep;
+    }
+    $schema_insert = str_replace($sep."$", "", $schema_insert);
+    $schema_insert = preg_replace("/\r\n|\n\r|\n|\r/", " ", $schema_insert);
+    $schema_insert .= "\t";
+    print(trim($schema_insert));
+    print "\n";
+}
+?>
