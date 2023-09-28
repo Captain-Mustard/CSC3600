fefaadbaaa49276b20c05a7f1b20fc4c447ae4a2
Ben McKenzie <u1058913@umail.usq.edu.au>
2023-09-28 23:20:13 +1000

    Update 'driver_platform/index.php'
    Fix session management

--- driver_platform/index.php
+++ driver_platform/index.php
@@ -3,30 +3,34 @@ require('../model/database.php');
 require('../model/driver_operations.php');
 session_start();
 
-// verifies the session type i.e. is driver or takes the user to the login
 $action = filter_input(INPUT_POST, 'action');
 if ($action === NULL) {
     $action = filter_input(INPUT_GET, 'action');
+    
     if ($action === NULL) {
-        if (isset($_SESSION['loggedin'])) {
-            if (isset($_SESSION['user_type']) && $_SESSION['user_type'] == "driver") {
-                $action = 'driver_display';
-                
-            } elseif (isset($_SESSION['user_type']) && $_SESSION['user_type'] != "driver") {
-                
-				if($_SESSION['user_type'] == "customer"){
-					
-					header('Location: ../customer_login/');
-				}else if($_SESSION['user_type'] == "analytics"){
-					
-					header('Location: ../analytics_login/');
-				}
+        // Check for specific session variables to determine if the user is logged in as a driver or another type
+        if (isset($_SESSION['user_type'])) {
+            switch ($_SESSION['user_type']) {
+                case "driver":
+                    $action = 'driver_display';
+                    break;
+                case "customer":
+                    header('Location: ../customer_login/');
+                    exit();
+                case "analytics":
+                    header('Location: ../analytics_login/');
+                    exit();
+                default:
+                    // Handle any other cases if necessary
+                    break;
+            }
         } else {
-            header('Location: ../driver_login/'); 
+            header('Location: ../driver_login/');
+            exit();
         }
     }
 }
-}
+
 // displays the drivers view
 if($action == 'driver_display'){
 
