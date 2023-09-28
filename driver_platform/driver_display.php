faab7a3412393c115d88f8069b4592852bbe5a8d
Laura Betts <u1125483@umail.usq.edu.au>
2023-09-28 13:56:34 +1000

    Latest working version

--- driver_platform/driver_display.php
+++ driver_platform/driver_display.php
@@ -1,25 +1,27 @@
 <?php include '../view/header.php'; ?>
 <main>
     <!-- basic output of the drivers page, lets them choose what day direction the bus heads in -->
-	<h2>Driver Page</h2>
-    <p>Driver Tools</p>
     <form action="." method="post">
 		<label for="destination">Choose a destination:</label>
-
-			<select id="destination" name="destination">
+		<select id="destination" name="destination">
 				<option value="toowoomba">Toowoomba</option>
 				<option value="Springfield">Springfield</option>
 			</select>
-			 <input type="hidden" name="action" value="list_busses">
-			</br>
+
+			</br> </br>
 			<label for="day">Choose a day:</label>
 			<input type="date"/>
 
+			<input type="hidden" name="action" value="list_busses">
 			<input type="submit" value="Go">
+</br>
+
+
 	</form>
 	<!-- logout button -->
+
     <form>
-		
+
 		  <input type="hidden" name="action" value="log_out">
                 
 			<input type="submit" value="Logout">
