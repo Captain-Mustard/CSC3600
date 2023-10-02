3d423da8825186aee8977e3b98078c28f974b5de
Laura Betts <u1125483@umail.usq.edu.au>
2023-10-03 06:26:42 +1000

    Added else-if statements to control flow of buttons
    Added else-if statements to control flow depending on route or day input and type of analytic button clicked.

--- analytics/index.php
+++ analytics/index.php
@@ -27,64 +27,91 @@ if ($action === NULL) {
     }
 }
 
+
 // displays the drivers view
 if($action == 'analytics'){
-    #Viewing Analytics by Route:
-    if($action == filter_input(INPUT_POST,'destination')){
-        
-        $destination = filter_input(INPUT_POST,'destination');
-
-        if($action == filter_input(INPUT_POST,'next_7_days')){
-             $bookings =  get_next_week_bookings_by_routes($destination);
-        }
-        else if($action == 'past_7_days'){
-            $bookings = get_last_week_bookings_by_routes($destination);
-        }
-        else if($action == 'past_30_days'){
-            $bookings = get_last_month_bookings_by_routes($destination);
-        }
-        else if($action == 'next_30_days'){
-            $bookings = get_next_month_bookings_by_routes($destination);
-        }
-    }
-
-    #Viewing Analytics by Day:
-    if($action == filter_input(INPUT_POST,'day')){
-
-        $day = filter_input(INPUT_POST,'day');
-
-        if($action== filter_input(INPUT_POST,'next_7_days')){
-            $bookings = get_next_week_bookings_by_day($day);
-            $passenger_count = get_next_week_passenger_count($day);
-        }
-        else if($action==filter_input(INPUT_POST,'past_7_days')){
-            $bookings = get_last_week_bookings_by_day($day);
-            $passenger_count = get_last_week_passenger_count($day);
-        }
-        else if($action ==filter_input(INPUT_POST,'past_30_days')){
-            $bookings = get_last_month_bookings_by_day($destination);
-            $passenger_count = get_last_month_passenger_count($day);
-    
-        }
-        else if($action == filter_input(INPUT_POST,'next_30_days')){
-            $bookings = get_next_month_bookings_by_day($day);
-            $passenger_count = get_next_month_passenger_count($day);
-        }
-
-    }
-
-    include('view_analytics.php');
+    include('analytics.php');
 }
 
-	
+elseif ($action == 'view_analytics'){
+
+    # if One of the analytics buttons clicked without any input, will get error message.
+    if (filter_input(INPUT_POST,'submit_start')==='' || filter_input(INPUT_POST,'submit_end')===''
+    || filter_input(INPUT_POST,'submit_day')===''){
+        $_POST['submit_start'] = NULL;
+        $_POST['submit_end'] = NULL;
+        $_POST['submit_day'] = NULL;
+    }
+    #If both the start and end destinations are set:
+    elseif (NULL!==(filter_input(INPUT_POST,'submit_start')) && (NULL!==(filter_input(INPUT_POST,'submit_end')))){
+        $print_start = filter_input(INPUT_POST,'start');
+        $print_end = filter_input(INPUT_POST,'end');
+        echo gettype($print_start), gettype($print_end);
+
+        $route_analytics;
+        $valid_input;
+
+    #If the day is set, then $day_analytics = True, $valid_input = True:
+    }elseif (null!==(filter_input(INPUT_POST,'submit_day'))){
+
+        $day_analytics;
+        $valid_input;
+
+    # Message requesting either 2 options of valid input.
+    }else{
+        Echo "Please select either a start and end location, or submit a day.";
+
+    }
+
+    if ($valid_input){
+        if($route_analytics){
+
+            $destination = filter_input(INPUT_POST,'end');
+
+            if ($action == 'next_7_days'){
+                $bookings =  get_next_week_bookings_by_routes($destination);
+            }
+            if ($action == 'past_7_days'){
+                $bookings = get_last_week_bookings_by_routes($destination);
+            }
+            if ($action == 'past_30_days'){
+                $bookings = get_last_month_bookings_by_routes($destination);
+            }
+            if ($action == 'next_30_days'){
+                $bookings = get_next_month_bookings_by_routes($destination);
+            }
+
+        }if($day_analytics){
+
+            $day = filter_input(INPUT_POST,'day');
+
+            if ($action == 'next_7_days'){
+                $bookings = get_next_week_bookings_by_day($day);
+                $passenger_count = get_next_week_passenger_count($day);
+            }
+            if ($action == 'past_7_days'){
+                $bookings = get_last_week_bookings_by_day($day);
+                $passenger_count = get_last_week_passenger_count($day);
+            }
+            if ($action == 'past_30_days'){
+                $bookings = get_last_month_bookings_by_day($destination);
+                $passenger_count = get_last_month_passenger_count($day);
+            }
+            if ($action == 'next_30_days'){
+                $bookings = get_next_month_bookings_by_day($day);
+                $passenger_count = get_next_month_passenger_count($day);   
+            }
+        }
+    }
+    
+}
 
 
 // code can be reused at each index page? for customers. Is there a better way?
-else if($action == 'log_out'){
+if ($action == 'log_out'){
 	session_destroy(); // This destroys the entire session
 	header('Location: ../analytics_login/');
 	
 }
-
 
 ?>
