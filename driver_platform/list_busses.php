a29273a3b08eecb3ec37dc453bab398908382e4e
Laura Betts <u1125483@umail.usq.edu.au>
2023-10-03 06:35:39 +1000

    Added CSS

--- driver_platform/list_busses.php
+++ driver_platform/list_busses.php
@@ -1,4 +1,7 @@
 <?php include '../view/header.php'; ?>
+<link rel="stylesheet" type="text/css" href="../main.css">
+
+<div class="container">
 <h1>Todays Trips</h1>
     <!-- display a table of the days trips -->
     <table>
@@ -12,17 +15,20 @@
             <th>&nbsp;</th>
         </tr>
         <?php foreach ($trips as $trip) : ?>
+            
         <tr>
             <td><?php echo htmlspecialchars($trip['busNo']); ?></td>
             <td><?php echo htmlspecialchars($trip['stopOneTime']); ?></td>
 			 <td><?php echo htmlspecialchars($trip['stopTwoTime']); ?></td>
             <td><?php echo htmlspecialchars($trip['stopThreeTime']); ?></td>
             <td><?php echo htmlspecialchars($trip['stopFourTime']); ?></td>
+
+
             <td><form action="." method="post">
                 
 				<input type="hidden" name="action"
                        value="get_passengers">
-                
+
 				<input type="hidden" name="bus_time"
                        value="<?php echo htmlspecialchars($trip['stopOneTime']); ?>">
 			    
@@ -32,7 +38,13 @@
                 <input type="submit" value="Get Passengers">
             </form></td>
         </tr>
+
         <?php endforeach; ?>
     </table>
+    <form action="../booking_platform/logout.php" method="post">
+            <input type="hidden" name="action" value="log_out">
+            <input class="submit-button" type="submit" value="Logout">
+        </form>
+        </div>
 	
 <?php include '../view/footer.php'; ?>
