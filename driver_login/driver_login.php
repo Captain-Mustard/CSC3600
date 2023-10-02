9b1cd63877826e68979ffa87af2fc8da1711ee88
Ben McKenzie <u1058913@umail.usq.edu.au>
2023-10-02 21:35:44 +1000

    Update 'driver_login/driver_login.php'
    Apply Style

--- driver_login/driver_login.php
+++ driver_login/driver_login.php
@@ -1,32 +1,30 @@
 <?php include '../view/header.php'; ?>
-<style>
-	
-</style>
 
 <main>
+    <div class="container">
+        <h2>Driver Login</h2>
 
-    <h2>Driver Login</h2>
-		
-    <!-- display a search form -->
-    <form action="." method="post">
-        <label for="driver_id">Driver Username:</label>
-			<input type="text" id="driver_id" name="driver_id" 
-			value="" required>
-		
-		<label for="password">Password:</label>
-			<input type="password" id="password" name="password" 
-			value="" required>
-		
-		
-		
-		
-			<input type="hidden" name="action" value="logged_in">
-                
-			<input type="submit" value="Login">
-    </form>
-	
+        <?php
+        // Check if an error message is set
+        if (isset($error)) {
+            echo '<div class="error-message">' . htmlspecialchars($error) . '</div>';
+        }
+        ?>
 
-	
-<?php include '../view/footer.php'; ?>
-	
+        <!-- Display a login form -->
+        <div class="form-container">
+            <form action="." method="post">
+                <label for="driver_id">Driver Username:</label>
+                <input type="text" id="driver_id" name="driver_id" value="" required>
+
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" value="" required>
+
+                <input type="hidden" name="action" value="logged_in">
+                <input type="submit" class="submit-button" value="Login">
+            </form>
+        </div>
+    </div>
+
+    <?php include '../view/footer.php'; ?>
 </main>
