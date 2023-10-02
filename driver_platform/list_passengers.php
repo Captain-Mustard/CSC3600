8f65f4f65086373b04a451913e0a032eb8e3d8c8
Laura Betts <u1125483@umail.usq.edu.au>
2023-10-03 06:34:29 +1000

    Added CSS

--- driver_platform/list_passengers.php
+++ driver_platform/list_passengers.php
@@ -1,18 +1,23 @@
-<?php include '../view/header.php'; ?>
+<?php include '../view/header.php'; 
+require('../model/database.php');?>
+
+
+<?php $passengers_on_trips = get_passengers_by_schedule_ID($schedule_id); ?>
+
+<div class="container">
 <h1>Passengers on Current Trip</h1>
     <!-- display a table of product -->
     <table>
         <tr>
             <th>UniSQ ID: </th>
             <th>Passenger Type: </th>
-			
+
             <th>&nbsp;</th>
         </tr>
         <?php foreach ($passengers_on_trips as $passengers_on_trip) : ?>
         <tr>
             <td><?php echo htmlspecialchars($passengers_on_trip['unisqId']); ?></td>
             <td><?php echo htmlspecialchars($passengers_on_trip['role']); ?></td>
-			<td><?php echo htmlspecialchars($passengers_on_trip['finished']); ?></td>
 
                 <td><form action="." method="post">
 
@@ -24,6 +29,10 @@
 
                 <input type="hidden" name="trip_id"
                 value="<?php echo htmlspecialchars($passengers_on_trip['tripId']);?>">
+
+                <input type="hidden" name="schedule_id"
+                value="<?php echo htmlspecialchars($passengers_on_trip['scheduleId']);?>">
+
               
                 <input type="hidden" name="action"
                        value="passenger_marked_off">
@@ -39,8 +48,14 @@
 
 
 
-    <form action="../booking_platform/new_booking.html" method="post">
+    <form action="../booking_platform/booking_add.php" method="post">
                 <input type="submit" value="Create New Booking:">
     </form>
+
+    <form action="../booking_platform/logout.php" method="post">
+            <input type="hidden" name="action" value="log_out">
+            <input class="submit-button" type="submit" value="Logout">
+        </form>
+        </div>
 	
 <?php include '../view/footer.php'; ?>
