e7550a0f6f39ab1233b4188958d9e88a5030a86d
Ben McKenzie <u1058913@umail.usq.edu.au>
2023-10-02 21:36:09 +1000

    Update 'index.php'
    Apply Style

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?php include 'view/header.php'; ?>
-<main>
-    <nav>
-        <h2>Admin & Analytics</h2>
-        <ul>
+
+<main class="home-main">
+    <nav class="home-nav">
+        <h2 class="section-title">Admin & Analytics</h2>
+        <ul class="section-links">
             <li><a href="analytics">Admin & Analytics</a></li>
         </ul>
 
-        <h2>Booking Platform</h2>
-        <ul>
+        <h2 class="section-title">Booking Platform</h2>
+        <ul class="section-links">
             <li><a href="booking_platform">End User Bookings</a></li>
         </ul>
 
-        <h2>Driver Platform</h2>
-        <ul>
+        <h2 class="section-title">Driver Platform</h2>
+        <ul class="section-links">
             <li><a href="driver_login">Bus Driver Admin</a></li>
         </ul>
     </nav>
 </main>
+
 <?php include 'view/footer.php'; ?>
