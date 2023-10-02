cb794cbe5f9ea0b95498f7dc2d4ce5db374ef48f
Laura Betts <u1125483@umail.usq.edu.au>
2023-10-03 06:30:53 +1000

    Added CSS, fixed form including links.

--- driver_platform/no_passengers_found.php
+++ driver_platform/no_passengers_found.php
@@ -1,15 +1,23 @@
 <?php include '../view/header.php'; ?>
+<div class="container">
 <main>
     <h1>No Passengers Bookings Found.
         <br><br>
         <p>Click below to make new booking or view other bookings.</p>
+       
     </h1>
-</br></br></br>
-    <form action="../booking_platform/new_booking.html">
-        <input type="submit" value="New Booking"/>
-</form>
 
-    <form href="driver_display">
-        <input type="submit" value="View Another Booking"/>
+    <form action="../booking_platform/booking_add.php">
+        <input class="submit-button" type="submit" value="New Booking"/>
+    </form>
+
+    <form action="../driver_platform">
+        <input class="submit-button" type="submit" value="View Another Booking"/>
+</form>
+        <form action="../booking_platform/logout.php" method="post">
+            <input type="hidden" name="action" value="log_out">
+            <input class="submit-button" type="submit" value="Logout">
+        </form>
 </main>
+</div>
 <?php include '../view/footer.php'; ?>
