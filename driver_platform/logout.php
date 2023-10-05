9581f1eac211aaab8bcf25d10cf77d07f83031c2
Ben McKenzie <u1058913@umail.usq.edu.au>
2023-10-05 12:17:42 +1000

    Add 'driver_platform/logout.php'

---
+++ driver_platform/logout.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+// Check if the user is logged in
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
+    // Unset all session variables
+    session_unset();
+
+    // Destroy the session
+    session_destroy();
+}
+
+// Redirect to the login page after logout
+header('Location: ../driver_login/');
+exit();
+?>
