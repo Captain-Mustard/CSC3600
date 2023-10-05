98c9cd054497d87a223f3d83e3769be322f17b94
Ben McKenzie <u1058913@umail.usq.edu.au>
2023-10-05 12:16:36 +1000

    Update 'driver_platform/list_passengers.php'

--- driver_platform/list_passengers.php
+++ driver_platform/list_passengers.php
@@ -1,61 +1,39 @@
-<?php include '../view/header.php'; 
-require('../model/database.php');?>
-
-
-<?php $passengers_on_trips = get_passengers_by_schedule_ID($schedule_id); ?>
-
 <div class="container">
-<h1>Passengers on Current Trip</h1>
-    <!-- display a table of product -->
+    <h1>Passengers on Bus <?php echo htmlspecialchars($busNo); ?> to <?php echo htmlspecialchars($destination); ?> Departing <?php echo htmlspecialchars($busTime); ?></h1>
     <table>
         <tr>
-            <th>UniSQ ID: </th>
-            <th>Passenger Type: </th>
-
-            <th>&nbsp;</th>
+            <th>UniSQ ID:</th>
+            <th>Passenger Type:</th>
+            <th>Off Time:</th>
+            <th>Finished:</th>
+            <th>Action:</th> <!-- Add Action column -->
         </tr>
-        <?php foreach ($passengers_on_trips as $passengers_on_trip) : ?>
-        <tr>
-            <td><?php echo htmlspecialchars($passengers_on_trip['unisqId']); ?></td>
-            <td><?php echo htmlspecialchars($passengers_on_trip['role']); ?></td>
-
-                <td><form action="." method="post">
-
-                <input type="hidden" name="uni_id"
-                value="<?php echo htmlspecialchars($passengers_on_trip['unisqId']);?>">
-
-                <input type="hidden" name="off_time"
-                value="<?php echo htmlspecialchars($passengers_on_trip['offTime']);?>">
-
-                <input type="hidden" name="trip_id"
-                value="<?php echo htmlspecialchars($passengers_on_trip['tripId']);?>">
-
-                <input type="hidden" name="schedule_id"
-                value="<?php echo htmlspecialchars($passengers_on_trip['scheduleId']);?>">
-
-              
-                <input type="hidden" name="action"
-                       value="passenger_marked_off">
-
-
-            <input type="submit" value="Check Off Bus">
-                 
-                
-            </form></td>
-        </tr>
+        <?php foreach ($passengers_on_trips as $passenger) : ?>
+            <tr>
+                <td><?php echo htmlspecialchars($passenger['unisqId']); ?></td>
+                <td><?php echo htmlspecialchars($passenger['role']); ?></td>
+                <td><?php echo ($passenger['offTime'] !== null) ? date('h:i A', strtotime(htmlspecialchars($passenger['offTime']))) : ''; ?></td>
+                <td><?php echo (htmlspecialchars($passenger['finished']) === '0') ? 'OFF' : ''; ?></td>
+                <td>
+                    <form action="." method="post">
+                        <input type="hidden" name="action" value="passenger_marked_off">
+                        <input type="hidden" name="uni_id" value="<?php echo htmlspecialchars($passenger['unisqId']); ?>">
+                        <input type="hidden" name="off_time" value="<?php echo date('Y-m-d H:i:s'); ?>"> <!-- Current time -->
+                        <input type="hidden" name="trip_id" value="<?php echo htmlspecialchars($passenger['tripId']); ?>">
+                        <input type="hidden" name="schedule_id" value="<?php echo htmlspecialchars($passenger['scheduleId']); ?>">
+                        <input type="hidden" name="busNo" value="<?php echo htmlspecialchars($busNo); ?>">
+                        <input type="hidden" name="bus_time" value="<?php echo htmlspecialchars($busTime); ?>">
+                        <input type="hidden" name="destination" value="<?php echo htmlspecialchars($destination); ?>">
+                        <input type="submit" value="Offload Passenger">
+                    </form>
+                </td>
+            </tr>
         <?php endforeach; ?>
     </table>
-
-
-
-    <form action="../booking_platform/booking_add.php" method="post">
-                <input type="submit" value="Create New Booking:">
+    <form action="list_busses.php" method="get">
+        <input type="submit" value="Back to Bus List">
     </form>
-
-    <form action="../booking_platform/logout.php" method="post">
-            <input type="hidden" name="action" value="log_out">
-            <input class="submit-button" type="submit" value="Logout">
-        </form>
-        </div>
-	
-<?php include '../view/footer.php'; ?>
+    <form action="new_booking_temp.php">
+        <input class="button" type="submit" value="Create New Booking"/>
+    </form>
+</div>
