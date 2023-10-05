09ec2e3031da229f1c2650902711760b65caa6a6
Jake Flynn <u1138451@umail.usq.edu.au>
2023-10-05 19:43:47 +1000

    fixed analytics count functions

--- Model/analytics.php
+++ Model/analytics.php
@@ -154,62 +154,66 @@ function get_last_month_bookings_by_day($day){
 
 
 // get passenger count each day for the last 7 days
-function get_last_week_passenger_count($day){
+function get_last_week_passenger_count(){
 	global $db;
     $query = '
-        SELECT SUM(b.numberOfPassengers) as totalPassengers
-        FROM Bookings AS b
-        WHERE b.bookingDate BETWEEN DATE_SUB(CURDATE(), INTERVAL 7 DAY) AND CURDATE()';
+        SELECT COUNT(DISTINCT b.scheduleId) as totalTrips
+		FROM BusTrips AS b
+		INNER JOIN BusSchedules AS s ON b.scheduleId = s.scheduleId
+		WHERE s.busDate BETWEEN DATE_SUB(CURDATE(), INTERVAL 7 DAY) AND CURDATE();';
     $statement = $db->prepare($query);
-	$statement->bindValue(':day', $day);
+	
 	$statement->execute();
-    $last_week_bookings = $statement->fetchAll();
+    $last_week_bookings = $statement->fetch();
     $statement->closeCursor();
     return $last_week_bookings;
 }
 
 // last 30 days
-function get_last_month_passenger_count($day){
+function get_last_month_passenger_count(){
 	global $db;
     $query = '
-        SELECT SUM(b.numberOfPassengers) as totalPassengers
-        FROM Bookings AS b
-        WHERE b.bookingDate BETWEEN DATE_SUB(CURDATE(), INTERVAL 30 DAY) AND CURDATE()';
+        SELECT COUNT(DISTINCT b.scheduleId) as totalTrips
+		FROM BusTrips AS b
+		INNER JOIN BusSchedules AS s ON b.scheduleId = s.scheduleId
+		WHERE s.busDate BETWEEN DATE_SUB(CURDATE(), INTERVAL 30 DAY) AND CURDATE();
+
+';
     $statement = $db->prepare($query);
-	$statement->bindValue(':day', $day);
 	$statement->execute();
-    $last_week_bookings = $statement->fetchAll();
+    $last_week_bookings = $statement->fetch();
     $statement->closeCursor();
     return $last_week_bookings;
 }
 
 // get passenger count each day for the next 7 days
-function get_next_week_passenger_count($day){
+function get_next_week_passenger_count(){
 	global $db;
     $query = '
-         SELECT COUNT(b.tripId) as totalPassengers
-        FROM BusTrips AS b
-        INNER JOIN BusSchedules AS s ON b.scheduleId = s.scheduleId
-        WHERE s.busDate BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 7 DAY)';
+         SELECT COUNT(DISTINCT b.scheduleId) as totalTrips
+			FROM BusTrips AS b
+			INNER JOIN BusSchedules AS s ON b.scheduleId = s.scheduleId
+			WHERE s.busDate BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 7 DAY)';
     $statement = $db->prepare($query);
-	$statement->bindValue(':day', $day);
+	
 	$statement->execute();
-    $last_week_bookings = $statement->fetchAll();
+    $last_week_bookings = $statement->fetch();
     $statement->closeCursor();
     return $last_week_bookings;
 }
 
-function get_next_month_passenger_count($day){
+function get_next_month_passenger_count(){
 	global $db;
     $query = '
-         SELECT COUNT(b.tripId) as totalPassengers
-        FROM BusTrips AS b
-        INNER JOIN BusSchedules AS s ON b.scheduleId = s.scheduleId
-        WHERE s.busDate BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 30 DAY)';
+         SELECT COUNT(DISTINCT b.scheduleId) as totalTrips
+			FROM BusTrips AS b
+			INNER JOIN BusSchedules AS s ON b.scheduleId = s.scheduleId
+			WHERE s.busDate BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 30 DAY)
+';
     $statement = $db->prepare($query);
-	$statement->bindValue(':day', $day);
+	
 	$statement->execute();
-    $last_week_bookings = $statement->fetchAll();
+    $last_week_bookings = $statement->fetch();
     $statement->closeCursor();
     return $last_week_bookings;
 }
