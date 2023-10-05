851acf54d9d10acfec5b155b5f393a864c15313e
Ben McKenzie <u1058913@umail.usq.edu.au>
2023-10-05 12:08:02 +1000

    Update 'Model/login_user.php'

--- Model/login_user.php
+++ Model/login_user.php
@@ -13,13 +13,12 @@ function get_passenger($uni_id) {
 }
 
 # Validate user login
-function login_check($username, $password) {
+function login_check($username) {
     global $db;
-    $query = 'SELECT unisq_Id FROM Passengers
-			 WHERE unisqId = :user_name AND password = :password';
+    $query = 'SELECT * FROM Passengers
+			 WHERE unisqId = :user_name';
     $statement = $db->prepare($query);
     $statement->bindValue(':user_name', $username);
-	$statement->bindValue(':password', $password);
     $statement->execute();
     $user = $statement->fetch();
     $statement->closeCursor();
@@ -43,13 +42,12 @@ function add_passenger($uni_id, $role, $email, $password) {
 }
 
 # Driver login
-function get_driver_login($driver_username, $password) {
+function get_driver_login($driver_username) {
     global $db;
     $query = 'SELECT * FROM BusDriver
-              WHERE driverUsername = :driver_username AND password = :password';
+              WHERE driverUsername = :driver_username';
     $statement = $db->prepare($query);
-    $statement->bindValue(':driver_username', $driver_username);
-	$statement->bindValue(':password', $password);
+	$statement->bindValue(':driver_username', $driver_username);
     $statement->execute();
     $driver = $statement->fetch();
     $statement->closeCursor();
@@ -57,16 +55,49 @@ function get_driver_login($driver_username, $password) {
 }
 
 # Metrics account login
-function get_metrics_account($metric_username, $password) {
+function get_metrics_account($metric_username) {
     global $db;
     $query = 'SELECT * FROM MetricsLogin
-              WHERE username = :metric_username AND password = :password';
+              WHERE username = :metric_username';
     $statement = $db->prepare($query);
-    $statement->bindValue(':metric_username', $metric_username);
-	$statement->bindValue(':password', $password);
+	$statement->bindValue(':metric_username', $metric_username);
     $statement->execute();
     $metric_login = $statement->fetch();
     $statement->closeCursor();
     return $metric_login;
+}
+
+# Insert new user
+function insert_new_passenger($uni_id, $role, $email, $password) {
+    global $db;
+    $query = 'INSERT INTO Passengers
+                 (unisqId, role, email, password)
+              VALUES
+                 (:uni_id, :role, :email, :password)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':uni_id', $uni_id);
+    $statement->bindValue(':role', $role);
+    $statement->bindValue(':email', $email);
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    $statement->bindValue(':password', $hashed_password);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+# Create a new driver
+function insert_new_bus_driver($driver_id, $driver_username, $email, $password) {
+    global $db;
+    $query = 'INSERT INTO BusDriver
+                 (driverId, driverUsername, email, password)
+              VALUES
+                 (:driver_id, :driver_username, :email, :password)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':driver_id', $driver_id);
+    $statement->bindValue(':driver_username', $driver_username);
+    $statement->bindValue(':email', $email);
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    $statement->bindValue(':password', $hashed_password);
+    $statement->execute();
+    $statement->closeCursor();
 }
 ?>
