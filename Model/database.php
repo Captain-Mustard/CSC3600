3bbd738feb278e64b2fda07aba5ff24396b4e824
Ben McKenzie <u1058913@umail.usq.edu.au>
2023-10-06 14:54:31 +1000

    Commented and Formatted

--- Model/database.php
+++ Model/database.php
@@ -1,13 +1,20 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=shuttle_bus';
-    $username = 'db_user';
-    $password = 'secure';
+// Database connection settings
+$dsn = 'mysql:host=localhost;dbname=shuttle_bus';
+$username = 'db_user';
+$password = 'secure';
 
-    try {
-        $db = new PDO($dsn, $username, $password);
-    } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        include('../errors/database_error.php');
-        exit();
-    }
+try {
+    // Attempt to create a PDO database connection
+    $db = new PDO($dsn, $username, $password);
+} catch (PDOException $e) {
+    // If an exception (error) occurs during connection, handle it
+    $error_message = $e->getMessage();
+
+    // Include an error handling script (you may need to specify the correct path)
+    include('../errors/database_error.php');
+
+    // Exit the script to prevent further execution in case of a database error
+    exit();
+}
 ?>
