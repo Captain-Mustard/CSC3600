54e0911a4a058d612f4ae81ae498d5da754d792b
Ben McKenzie <u1058913@umail.usq.edu.au>
2023-10-06 14:55:22 +1000

    Commented and Formatted

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
-<?php include 'view/header.php'; ?>
+<?php
+// Include the header.php file to add common header content
+include 'view/header.php';
+?>
 
 <main class="home-main">
     <nav class="home-nav">
+        <!-- Admin & Analytics Section -->
         <h2 class="section-title">Admin & Analytics</h2>
         <ul class="section-links">
             <li><a href="analytics">Admin & Analytics</a></li>
             <li><a href="analytics_testing">Analytics Testing</a></li>
         </ul>
 
+        <!-- Booking Platform Section -->
         <h2 class="section-title">Booking Platform</h2>
         <ul class="section-links">
             <li><a href="booking_platform">End User Bookings</a></li>
         </ul>
 
+        <!-- Driver Platform Section -->
         <h2 class="section-title">Driver Platform</h2>
         <ul class="section-links">
             <li><a href="driver_login">Bus Driver Admin</a></li>
@@ -20,4 +26,7 @@
     </nav>
 </main>
 
-<?php include 'view/footer.php'; ?>
+<?php
+// Include the footer.php file to add common footer content
+include 'view/footer.php';
+?>
