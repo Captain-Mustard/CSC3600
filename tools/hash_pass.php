ac09c7023288ad9247fc161efb4028f1b71158de
Ben McKenzie <u1058913@umail.usq.edu.au>
2023-10-06 14:54:56 +1000

    Commented and Formatted

--- tools/hash_pass.php
+++ tools/hash_pass.php
@@ -1,7 +1,7 @@
 <?php
-
+// Generate a hashed password using the PASSWORD_DEFAULT algorithm
 $make_hashed_password = password_hash("test1", PASSWORD_DEFAULT);
 
+// Output the hashed password
 echo $make_hashed_password;
-
 ?>
