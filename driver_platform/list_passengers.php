bfa6a01a00e08fde5fd1787065c6e98940e66bb9
Ben McKenzie <u1058913@umail.usq.edu.au>
2023-10-06 14:53:58 +1000

    Commented and Formatted

--- driver_platform/list_passengers.php
+++ driver_platform/list_passengers.php
@@ -1,19 +1,32 @@
 <div class="container">
-    <h1>Passengers on Bus <?php echo htmlspecialchars($busNo); ?> to <?php echo htmlspecialchars($destination); ?> Departing <?php echo htmlspecialchars($busTime); ?></h1>
+    <h1>
+        Passengers on Bus <?php echo htmlspecialchars($busNo); ?> to <?php echo htmlspecialchars($destination); ?>
+        Departing <?php echo htmlspecialchars($busTime); ?>
+    </h1>
     <table>
         <tr>
-            <th>UniSQ ID:</th>
-            <th>Passenger Type:</th>
-            <th>Off Time:</th>
-            <th>Finished:</th>
-            <th>Action:</th> <!-- Add Action column -->
+            <th>UniSQ ID</th>
+            <th>Passenger Type</th>
+            <th>Off Time</th>
+            <th>Finished</th>
+            <th>Action</th>
         </tr>
         <?php foreach ($passengers_on_trips as $passenger) : ?>
             <tr>
                 <td><?php echo htmlspecialchars($passenger['unisqId']); ?></td>
                 <td><?php echo htmlspecialchars($passenger['role']); ?></td>
-                <td><?php echo ($passenger['offTime'] !== null) ? date('h:i A', strtotime(htmlspecialchars($passenger['offTime']))) : ''; ?></td>
-                <td><?php echo (htmlspecialchars($passenger['finished']) === '0') ? 'OFF' : ''; ?></td>
+                <td>
+                    <?php
+                    // Display the off time, if available
+                    echo ($passenger['offTime'] !== null) ? date('h:i A', strtotime(htmlspecialchars($passenger['offTime']))) : '';
+                    ?>
+                </td>
+                <td>
+                    <?php
+                    // Display "OFF" if the passenger is marked as finished
+                    echo (htmlspecialchars($passenger['finished']) === '0') ? 'OFF' : '';
+                    ?>
+                </td>
                 <td>
                     <form action="." method="post">
                         <input type="hidden" name="action" value="passenger_marked_off">
